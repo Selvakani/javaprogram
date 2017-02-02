61f82800bd8bebf83f102704aa5cda7a1cda57b4
Selvakani <selvakani267@gmail.com>
2017-02-02 15:12:10 +0530

    No change

---
+++ oddeven.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		  int a;
+		  Scanner num=new Scanner(System.in);
+		  System.out.println("enter a number:");
+		  a = num.nextInt();
+		  if(a%2==0)
+		  {
+		  	System.out.println(""+ a +" is even");
+		  }
+		  else
+		  {
+		  	System.out.println(""+ a +" is odd");
+		  }
+		  }
+}
